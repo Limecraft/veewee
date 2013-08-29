25be47959868f484fa4d6b8c7ce3f316668b1ca6
Wim Van Lancker <wim.vanlancker@limecraft.com>
2013-08-29 12:00:13 +0200

    patch for cygwin + template

---
+++ templates/ubuntu-12.04.3-server-amd64/chef.sh
@@ -0,0 +1,8 @@
+# Install Chef
+#curl -L https://www.opscode.com/chef/install.sh | sudo bash
+
+# Install Chef
+wget https://www.opscode.com/chef/install.sh 
+chmod +x install.sh
+sudo ./install.sh -v 10.18.2
+
---
+++ templates/ubuntu-12.04.3-server-amd64/provisioner.sh
@@ -0,0 +1,14 @@
+date > /etc/vagrant_box_build_time
+
+# Setup sudo to allow no-password sudo for "sudo"
+useradd -s /bin/bash -m -G sudo provisioner
+
+usermod -a -G sudo provisioner
+
+# Installing vagrant keys
+mkdir /home/provisioner/.ssh
+chmod 700 /home/provisioner/.ssh
+cd /home/provisioner/.ssh
+wget --no-check-certificate 'http://boreas.limecraft.com/id_rsa.pub' -O authorized_keys
+chmod 600 /home/provisioner/.ssh/authorized_keys
+chown -R provisioner /home/provisioner/.ssh
