98d15267ec9a375470c815aa86bde689b4156ef3
wvlancke <wim.vanlancker@limecraft.com>
2013-10-31 12:01:02 +0100

    update

--- templates/ubuntu-12.04.3-server-amd64/definition.rb
+++ templates/ubuntu-12.04.3-server-amd64/definition.rb
@@ -1,7 +1,7 @@
 Veewee::Session.declare({
   :cpu_count => '2',
   :memory_size=> '4048',
-  :disk_size => '10140',
+  :disk_size => '402560',
   :disk_format => 'VDI',
   :hostiocache => 'off',
   :os_type_id => 'Ubuntu_64',
--- templates/ubuntu-12.04.3-server-amd64/provisioner.sh
+++ templates/ubuntu-12.04.3-server-amd64/provisioner.sh
@@ -12,3 +12,6 @@ cd /home/provisioner/.ssh
 wget --no-check-certificate 'http://boreas.limecraft.com/id_rsa.pub' -O authorized_keys
 chmod 600 /home/provisioner/.ssh/authorized_keys
 chown -R provisioner /home/provisioner/.ssh
+
+echo 'provisioner ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/provisioner
+echo 'Defaults    env_keep += "SSH_AUTH_SOCK"' >> /etc/sudoers.d/provisioner
